227fdfcc2c85007cfc4e4f95487e921db2683a9e
ajchao <angelachao371@gmail.com>
2019-12-01 19:31:25 -0500

    changes to matlab

--- runtest.m
+++ runtest.m
@@ -1,15 +1,15 @@
 function[armplanlength,time,cost,vertices] = runtest(mapfile, armstart, armgoal, planner_id)
 
-envmap = load(mapfile);
+load(mapfile, 'map');
 close all;
 
 %armplan should be a matrix of D by N 
 %where D is the number of DOFs in the arm (length of armstart) and
 %N is the number of steps in the plan 
-[armplan, armplanlength,time,cost,vertices] = armplanner(envmap, armstart, armgoal, planner_id); 
+[armplan, armplanlength,time,cost,vertices] = armplanner(map, armstart, armgoal, planner_id); 
 fprintf(1, 'plan of length %d was found\n', size(armplan,1));
 
 %Animate
-animate(envmap, armplan, armplanlength);
+animate(map, armplan, armplanlength);
 
 end
