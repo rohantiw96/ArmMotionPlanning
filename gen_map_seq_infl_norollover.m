17f1db68a7c475eb4868d5da0d6994ac632e4c3f
ajchao <angelachao371@gmail.com>
2019-12-05 18:49:43 -0500

    moving object in middle

---
+++ gen_map_seq_infl_norollover.m
@@ -0,0 +1,39 @@
+function [envmap] = gen_map_seq_infl(mapsize, obstaclesize, actionvec, startx, starty,inflation)
+
+envmap = zeros(mapsize, mapsize, length(actionvec));
+curr_pos = [startx-inflation, starty-inflation];
+obstaclesize= obstaclesize + 2*inflation*ones(1,2);
+for idx=1:length(actionvec)
+    obstacle_end_x = curr_pos(1) + obstaclesize(1)-1+2*inflation;
+    obstacle_end_y = curr_pos(2) + obstaclesize(2)-1+2*inflation;
+    
+    % check if object goes off the map
+    if(obstacle_end_x > mapsize)
+        obstacle_end_x = mapsize;
+    end
+    if(obstacle_end_y > mapsize)
+        obstacle_end_y = mapsize;
+    end
+    
+    % plot
+    envmap(curr_pos(2):obstacle_end_y, curr_pos(1):obstacle_end_x, idx) = 1;
+    
+    % update current position of obstacle
+    curr_pos = curr_pos + actionvec(idx,:);
+    
+    if(any(curr_pos <= 0))  % if position goes off at the 0-border:
+        jdx = find(curr_pos <= 0);
+        for i=1:length(jdx)
+            curr_pos(jdx(i)) = mapsize;
+        end
+        
+    elseif(any(curr_pos > mapsize)) % if position goes off at the end
+        jdx = find(curr_pos > mapsize);
+        for i=1:length(jdx)
+            curr_pos(jdx(i)) = 1;
+        end
+    end
+    
+end
+
+end
