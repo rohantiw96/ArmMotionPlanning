a4024bc271c898a66fac447bf304ecda0c2dcb6b
ajchao <angelachao371@gmail.com>
2019-12-06 00:08:06 -0500

    map with multiple moving objects

--- gen_object_norollover.m
+++ gen_object_norollover.m
@@ -1,11 +1,11 @@
-function [envmap] = gen_map_seq_infl_norollover(mapsize, obstaclesize, actionvec, startx, starty,inflation)
+function [map] = gen_object_norollover(map, obstaclesize, actionvec, startx, starty)
 
-envmap = zeros(mapsize, mapsize, length(actionvec));
-curr_pos = [startx-inflation, starty-inflation];
-obstaclesize= obstaclesize + 2*inflation*ones(1,2);
+curr_pos = [startx, starty];
+mapsize = size(map,1);
+
 for idx=1:length(actionvec)
-    obstacle_end_x = curr_pos(1) + obstaclesize(1)-1+2*inflation;
-    obstacle_end_y = curr_pos(2) + obstaclesize(2)-1+2*inflation;
+    obstacle_end_x = curr_pos(1) + obstaclesize(1)-1;
+    obstacle_end_y = curr_pos(2) + obstaclesize(2)-1;
     
     % check if object goes off the map
     if(obstacle_end_x > mapsize)
@@ -16,7 +16,7 @@ for idx=1:length(actionvec)
     end
     
     % plot
-    envmap(curr_pos(2):obstacle_end_y, curr_pos(1):obstacle_end_x, idx) = 1;
+    map(curr_pos(2):obstacle_end_y, curr_pos(1):obstacle_end_x, idx) = 1;
     
     % update current position of obstacle
     curr_pos = curr_pos + actionvec(idx,:);
